17198faf37a5e26e42cb761e629408ba92f49801
shad0wsync <jaysmith1107@gmail.com>
2026-07-31 09:39:55 -0500

    commit

---
+++ Scripts/Macs/SetDock.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+dockutilbin=$(/user/bin/dockutil)
+loggedInUser=$(echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }')
+loggedInUserPlist="/Users/$loggedInUser/Library/Preferences/com.apple.dock.plist"
+
+#Remove all from Dock
+$dockutilbin --remove all --no-restart $loggedInUserPlist; sleep 2
+
+#Add Applications to Dock
+
+#Add Microsoft Teams
+$dockutilbin --add file:///Applications/Microsoft\ Teams.app --no-restart $loggedInUserPlist
+
+#Add Microsoft Outlook
+$dockutilbin --add file:///Applications/Microsoft\ Outlook.app --no-restart $loggedInUserPlist
+
+#Add Microsoft Word
+$dockutilbin --add file:///Applications/Microsoft\ Word.app --no-restart $ loggedInUserPlist
+
+#Add Microsoft Excel
+$dockutilbin --add file:///Applications/Microsoft\ Excel.app --no-restart $ loggedInUserPlist
+
+#Add Microsoft PowerPoint
+$dockutilbin --add file:///Applications/Microsoft\ PowerPoint.app --no-restart $loggedInUserPlist
+
+#Add System Preferences
+$dockutilbin --add file:///Applications/System\ Preferences.app --no-restart $loggedInUserPlist
+
+#Add Downloads folder
+$dockutilbin --add '~/Downloads' --view grid --display folder $loggedInUserPlist
